486e7679a8e4cc6b916a7e616007113f9e1975bb
Gustavo Oliveira <Gustavo Oliveira>
2016-05-30 01:34:35 -0300

    criado nova pagina reset para resetar senha utilizando token recebido por email

---
+++ js/reset.js
@@ -0,0 +1,35 @@
+﻿function resetPassword() {
+    var password = $('#password')[0].value;
+    var confirm = $('#confirm')[0].value;
+    var token = window.location.search.substring(1).split('=')[1];
+
+    if ((password != null && confirm != null && password != '' && confirm != '') && (password == confirm)) {
+        var obj = {
+            password: password,
+            token: token
+        };
+
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:3000/receiveToken',
+            data: obj,
+            success: function (r, o) {
+                $('#password')[0].value = null;
+                $('#confirm')[0].value = null;
+                if (r.success != null) {
+                    alert(r.success);
+                    window.location.search = null;
+                } else {
+                    alert(r.error);
+                }
+                
+                console.log(r);
+            },
+            dataType: 'json'
+        });
+
+    } else {
+        alert('Senhas inválidas');
+    }
+
+}
