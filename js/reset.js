1ef9376c580db96fd92f518dee698fcad5d62e10
Gustavo Oliveira <Gustavo Oliveira>
2016-05-30 20:08:08 -0300

    alteração da url para enviar token

--- js/reset.js
+++ js/reset.js
@@ -11,7 +11,7 @@
 
         $.ajax({
             type: "POST",
-            url: 'http://localhost:3000/receiveToken',
+            url: 'http://api.artmanager.com.br/receiveToken',
             data: obj,
             success: function (r, o) {
                 $('#password')[0].value = null;
